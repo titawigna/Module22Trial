4d2989285944f062c361c9a81a4f02b5f7173b72
Wigna Pratita <89175246+titawigna@users.noreply.github.com>
2023-12-18 14:30:11 +0700

    Update LoginRunner.java
    deleting json report

--- CucumberLiveSess/src/test/java/runner/LoginRunner.java
+++ CucumberLiveSess/src/test/java/runner/LoginRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         glue = {"stepDef"}, //dimana step definition disimpan
         features = {"src/test/java/features"}, //dimana feature file disimpan
-        plugin = {"pretty", "html:reports/cucumber.html", "json:reports/cucumber.json"},
+        plugin = {"pretty", "html:reports/cucumber.html"},
         //menambahkan informasi mengenai step yg dieksekusi,
         //"html:reports/cucumber.html", "json:reports/cucumber.json
         tags = "@login-valid-emailandpass"
